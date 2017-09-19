4a8196eafd5215e18dd7a58a8b8a323374a902f2
Loc Doan <loc.doan@rockship.co>
2017-09-19 15:11:26 +0700

    Clean up

--- coupon.py
+++ coupon.py
@@ -7,21 +7,15 @@ import os, sys
 
 keywords = [u'ăn trưa','an trua','tra sua', u'trà sữa',u'cơm trưa', 'com trua', u'cơm', 'com', u'gà', 'ga']
 
-req = requests.get('https://www.deliverynow.vn/Offer/LoadMore?pageIndex=0&provinceId=217')
-coupon_info = req.json()
+for i in range(0,9):
+    url = 'https://www.deliverynow.vn/Offer/LoadMore?pageIndex=' + str(i) + '&provinceId=217'
+    req = requests.get(url)
+    coupon_info = req.json()
 
-
-req = requests.get('https://www.deliverynow.vn/Offer/LoadMore?pageIndex=0&provinceId=217')
-coupon_info = req.json()
-
-
-input_dict = coupon_info
-output_dict = [x:  for x in input_dict['data'] if (x['Restaurant']['FullAddress'][-6:]==u"Quận 3") or (x['Restaurant']['FullAddress'][-15:]==u"Quận Bình Thạnh")]
-{k: v for k, v in input_dict['data'].iteritems() if (['Restaurant']['FullAddress'][-6:]==u"Quận 3") or (['Restaurant']['FullAddress'][-15:]==u"Quận Bình Thạnh")}
-print type(input_dict)
-print type(output_dict)
-
-for data in output_dict['data']:
-    print data['Title'][:8]
-    print data['Restaurant']['ResName']
-    print data['PromoCode']
+    for data in coupon_info['data']:
+         if (data['Restaurant']['FullAddress'][-6:] == u"Quận 2") or (
+             data['Restaurant']['FullAddress'][-15:] == u"Quận Bình Thạnh"):
+             print data['Title'][:8]
+             print data['Restaurant']['ResName']
+             print data['PromoCode']
+             print data['Restaurant']['FullAddress']
--- mybot.py
+++ mybot.py
@@ -35,21 +35,33 @@ def morning(user, message):
 def morning_greeting():
     gendo.speak(u"Chào mọi người", "#random")
 
-@gendo.listen_for('eat')
+@gendo.listen_for('test')
 # @gendo.cron('*/1 * * * *')
 def cronjob():
     gendo.speak("Heyy, new discount yooo:\n","#random")
-    req = requests.get('https://www.deliverynow.vn/Offer/LoadMore?pageIndex=0&provinceId=217')
-    coupon_info = req.json()
-    for data in coupon_info['data']:
-        gendo.speak("Discount: " + data['Title'][4: 8] + "\n" + "Restaurant: " + data['Restaurant']['ResName'] + "\n" + "Code: " + data['PromoCode'] + "\n" + "Link: " + 'https://www.deliverynow.vn' + data['DetailUrl'],"#random")
+    for i in range(0, 9):
+        url = 'https://www.deliverynow.vn/Offer/LoadMore?pageIndex=' + str(i) + '&provinceId=217'
+        req = requests.get(url)
+        coupon_info = req.json()
 
-@gendo1.listen_for('bot ')
+        for data in coupon_info['data']:
+            if (data['Restaurant']['FullAddress'][-6:] == u"Quận 2") or (
+                        data['Restaurant']['FullAddress'][-15:] == u"Quận Bình Thạnh"):
+
+                discount = "Discount: " + data['Title'][4: 8] + "\n"
+                res_name = "Restaurant: " + data['Restaurant']['ResName'] + "\n"
+                code = "Code: " + data['PromoCode'] + "\n"
+                address = "Address: " + data['Restaurant']['FullAddress'] + "\n"
+                link = "Link: " + 'https://www.deliverynow.vn' + data['DetailUrl']
+
+                gendo.speak( discount  + res_name + code + address + link,"#random")
+
+@gendo.listen_for('bot ')
 def morning(user, message):
     # make sure message is "morning" and doesn't just contain it.
     if message.strip() == 'hey':
         return "helu @{user.username}"
 
 
-if __name__ == '__main__':
-    gendo.run()
+# if __name__ == '__main__':
+gendo.run()
